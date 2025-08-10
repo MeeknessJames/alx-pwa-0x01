fc2947083a6c6fd5070c700c9e42d4fac9958003
Meekness James <meeknessjames1@gmail.com>
2025-08-10 09:11:11 +0100

    feat: bootstrap alx-movie-app with Next.js, TypeScript, Tailwind and base components

---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieCard: React.FC = () => {
+  return (
+    <div className="border rounded p-4">
+      <h2>Movie Title</h2>
+      <p>Movie description here...</p>
+    </div>
+  );
+};
+
+export default MovieCard;
