b29352b02b9987db61fafc59f8175e22ab3f0933
Meekness James <meeknessjames1@gmail.com>
2025-08-24 07:03:50 +0100

    updated task

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -4,9 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        { }
         <link rel="manifest" href="/manifest.json" />
-        { }
         <meta name="theme-color" content="#0070f3" />
       </Head>
       <body className="antialiased">
